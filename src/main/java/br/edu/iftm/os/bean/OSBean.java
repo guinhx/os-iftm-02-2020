75629fd48354992a05084495a7a6618a9ff1326d
Diego Santos <diegosantosaraujo@outlook.com>
2021-01-30 01:04:58 -0300

    Implementando OS

--- src/main/java/br/edu/iftm/os/bean/OSBean.java
+++ src/main/java/br/edu/iftm/os/bean/OSBean.java
@@ -1,12 +1,18 @@
 package br.edu.iftm.os.bean;
 
+import br.edu.iftm.os.logic.ClienteLogic;
 import br.edu.iftm.os.logic.OSLogic;
+import br.edu.iftm.os.model.Cliente;
 import br.edu.iftm.os.model.OS;
+import br.edu.iftm.os.util.exception.ErroNegocioException;
+import br.edu.iftm.os.util.exception.ErroSistemaException;
 import lombok.Getter;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
 
 @Named
 @SessionScoped
@@ -16,6 +22,9 @@ public class OSBean extends CrudBean<OS, OSLogic>{
     @Inject
     private OSLogic logic;
 
+    @Inject
+    private ClienteLogic clienteLogic;
+
     public OSBean() {
         super(OS.class);
     }
@@ -23,5 +32,14 @@ public class OSBean extends CrudBean<OS, OSLogic>{
     @Override
     public OSLogic getLogic() {
         return this.logic;
+    }
+
+    public List<Cliente> getClientes() throws ErroNegocioException {
+        try {
+            return clienteLogic.buscar(null);
+        } catch (ErroSistemaException ex) {
+            addMensagemErro(ex.getMessage());
+            return new ArrayList<>();
+        }
     }
 }
